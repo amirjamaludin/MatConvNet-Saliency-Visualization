144e356269d68925ec3ea20609ae5f7af9d2f2fa
Amir Jamaludin <amirjamaludin@users.noreply.github.com>
2016-12-01 23:54:37 +0000

    Add files via upload

---
+++ matlab/+dagnn/Conv.m
@@ -0,0 +1,94 @@
+classdef Conv < dagnn.Filter
+  properties
+    size = [0 0 0 0]
+    hasBias = true
+    opts = {'cuDNN'}
+    exBackprop = false
+  end
+
+  methods
+    function outputs = forward(obj, inputs, params)
+      if ~obj.hasBias, params{2} = [] ; end
+      outputs{1} = vl_nnconv(...
+        inputs{1}, params{1}, params{2}, ...
+        'pad', obj.pad, ...
+        'stride', obj.stride, ...
+        'dilate', obj.dilate, ...
+        obj.opts{:}) ;
+    end
+
+    function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
+        if ~obj.hasBias, params{2} = [] ; end
+        if(~obj.exBackprop)
+            [derInputs{1}, derParams{1}, derParams{2}] = vl_nnconv(...
+                inputs{1}, params{1}, params{2}, derOutputs{1}, ...
+                'pad', obj.pad, ...
+                'stride', obj.stride, ...
+                'dilate', obj.dilate, ...
+                obj.opts{:}) ;
+        else            
+            wPlus = max(0,params{1});
+            b = params{2};
+
+            Abot = inputs{1};
+            Ptop = derOutputs{1};
+
+            % Forward Pass
+            Atop = vl_nnconv(...
+                Abot, wPlus, [], ...
+                'pad', obj.pad, ...
+                'stride', obj.stride, ...
+                'dilate', obj.dilate, ...
+                obj.opts{:}) ;
+            
+            Y = gpuArray(zeros(size(Atop),'single'));
+            nonZeroXIdx = logical(gather(Atop ~= 0));
+            Y(nonZeroXIdx) = Ptop(nonZeroXIdx) ./ Atop(nonZeroXIdx);
+            
+            % Backward Pass
+            [Pbot, derParams{1}, derParams{2}] = vl_nnconv(...
+                Abot, wPlus, [], Y, ...
+                'pad', obj.pad, ...
+                'stride', obj.stride, ...
+                'dilate', obj.dilate, ...
+                obj.opts{:}) ;
+            
+            derInputs{1} = Abot .* Pbot;
+        end
+    end
+
+    function kernelSize = getKernelSize(obj)
+      kernelSize = obj.size(1:2) ;
+    end
+
+    function outputSizes = getOutputSizes(obj, inputSizes)
+      outputSizes = getOutputSizes@dagnn.Filter(obj, inputSizes) ;
+      outputSizes{1}(3) = obj.size(4) ;
+    end
+
+    function params = initParams(obj)
+      % Xavier improved
+      sc = sqrt(2 / prod(obj.size(1:3))) ;
+      %sc = sqrt(2 / prod(obj.size([1 2 4]))) ;
+      params{1} = randn(obj.size,'single') * sc ;
+      if obj.hasBias
+        params{2} = zeros(obj.size(4),1,'single') ;
+      end
+    end
+
+    function set.size(obj, ksize)
+      % make sure that ksize has 4 dimensions
+      ksize = [ksize(:)' 1 1 1 1] ;
+      obj.size = ksize(1:4) ;
+    end
+
+    function obj = Conv(varargin)
+      obj.load(varargin) ;
+      % normalize field by implicitly calling setters defined in
+      % dagnn.Filter and here
+      obj.size = obj.size ;
+      obj.stride = obj.stride ;
+      obj.pad = obj.pad ;
+    end
+  end
+end
---
+++ matlab/+dagnn/Pooling.m
@@ -0,0 +1,64 @@
+classdef Pooling < dagnn.Filter
+  properties
+    method = 'max'
+    poolSize = [1 1]
+    opts = {'cuDNN'}
+    exBackprop = false
+  end
+
+  methods
+    function outputs = forward(self, inputs, params)
+      outputs{1} = vl_nnpool(inputs{1}, self.poolSize, ...
+                             'pad', self.pad, ...
+                             'stride', self.stride, ...
+                             'method', self.method, ...
+                             self.opts{:}) ;
+    end
+
+    function [derInputs, derParams] = backward(self, inputs, params, derOutputs)
+        if(~self.exBackprop)
+            derInputs{1} = vl_nnpool(inputs{1}, self.poolSize, derOutputs{1}, ...
+                'pad', self.pad, ...
+                'stride', self.stride, ...
+                'method', self.method, ...
+                self.opts{:}) ;
+            derParams = {} ;
+        else
+            Abot = inputs{1};
+            Ptop = derOutputs{1};
+            
+            Atop = vl_nnpool(Abot, self.poolSize, ...
+                'pad', self.pad, ...
+                'stride', self.stride, ...
+                'method', self.method, ...
+                self.opts{:}) ;
+            
+            Y = gpuArray(zeros(size(Atop),'single'));
+            nonZeroXIdx = logical(gather(Atop ~= 0));
+            Y(nonZeroXIdx) = Ptop(nonZeroXIdx) ./ Atop(nonZeroXIdx);
+            
+            Pbot = vl_nnpool(Abot, self.poolSize, Y, ...
+                'pad', self.pad, ...
+                'stride', self.stride, ...
+                'method', self.method, ...
+                self.opts{:}) ;
+
+            derInputs{1} = Abot .* Pbot;
+            derParams = {} ;
+        end
+    end
+
+    function kernelSize = getKernelSize(obj)
+      kernelSize = obj.poolSize ;
+    end
+
+    function outputSizes = getOutputSizes(obj, inputSizes)
+      outputSizes = getOutputSizes@dagnn.Filter(obj, inputSizes) ;
+      outputSizes{1}(3) = inputSizes{1}(3) ;
+    end
+
+    function obj = Pooling(varargin)
+      obj.load(varargin) ;
+    end
+  end
+end
